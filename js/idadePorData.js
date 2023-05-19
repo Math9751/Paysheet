03b17a2b784a4cbdecd3192cde5e0654ab22923c
Math9751 <matthewbelt.mc@gmail.com>
2023-05-18 22:00:04 -0300

    teste commit

---
+++ js/idadePorData.js
@@ -0,0 +1,15 @@
+function ageCount() {
+    var now =new Date();                            //getting current date
+    var currentY= now.getFullYear();                //extracting year from the date
+    var currentM= now.getMonth();                   //extracting month from the date
+      
+    var dobget =document.getElementById("birthday").value; //getting user input
+    var dob= new Date(dobget);                             //formatting input as date
+    var prevY= dob.getFullYear();                          //extracting year from input date
+    var prevM= dob.getMonth();                             //extracting month from input date
+      
+    var ageY =currentY - prevY;
+    var ageM =Math.abs(currentM- prevM);          //converting any negative value to positive
+      
+    document.getElementById('demo').innerHTML = ageY +' anos ';
+    }
